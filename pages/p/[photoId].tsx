c144f211a38c295436bda9f07f44de00421dc5ad
David Choi <davidgumzchoi@gmail.com>
2023-06-22 00:13:58 -0700

    high res public_id sort

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -61,7 +61,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export async function getStaticPaths() {
   const results = await cloudinary.v2.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by('public_id', 'desc')
+    .sort_by('public_id', 'asc')
     .max_results(200)
     .execute();
 
