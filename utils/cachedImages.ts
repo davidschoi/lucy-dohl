76279c194a6adcb3fa3a236da025412e2bc4f3a4
David Choi <1777321+davidschoi@users.noreply.github.com>
2025-08-27 12:54:54 -0700

    fix: handle missing cloudinary env

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -4,8 +4,25 @@ let cachedResults;
 
 export default async function getResults() {
   if (!cachedResults) {
+    const {
+      NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      CLOUDINARY_API_KEY,
+      CLOUDINARY_API_SECRET,
+      CLOUDINARY_FOLDER,
+    } = process.env;
+
+    if (
+      !NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ||
+      !CLOUDINARY_API_KEY ||
+      !CLOUDINARY_API_SECRET ||
+      !CLOUDINARY_FOLDER
+    ) {
+      cachedResults = { resources: [] };
+      return cachedResults;
+    }
+
     const fetchedResults = await cloudinary.v2.search
-      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+      .expression(`folder:${CLOUDINARY_FOLDER}/*`)
       .sort_by('public_id', 'asc')
       .max_results(200)
       .execute();
